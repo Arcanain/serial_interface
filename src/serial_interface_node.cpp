055a540beb88d79b0a7e83d8c6cde764036c6646
Ramune6110 <arduino6110@gmail.com>
2021-06-05 09:27:36 -0700

    [add]serial_interface.cpp

---
+++ src/serial_interface_node.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+#include "../include/SerialInterface.hpp"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "serial_interface_node");
+    ros::NodeHandle n;
+    
+}
