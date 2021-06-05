821c1ca366d5c4e9b60c220679f5e645a773f6d5
RyosukeMatsushima <m.ryosuke@me.com>
2021-06-05 19:48:44 +0900

    format

--- include/SerialDriver.hpp
+++ include/SerialDriver.hpp
@@ -1,17 +1,17 @@
 #ifndef _SERIAL_DRIVER_H_
 #define _SERIAL_DRIVER_H_
 
-#define SD_SERIAL_SPEED      115200
-#define SD_INVALID_HANDLE    (-1)
-#define SD_MAX_PORT_NAME_LEN  24
+#define SD_SERIAL_SPEED         115200
+#define SD_INVALID_HANDLE       (-1)
+#define SD_MAX_PORT_NAME_LEN    24
 
-#define SD_ERROR            (-1)
-#define SD_SUCCESS            0
+#define SD_ERROR                (-1)
+#define SD_SUCCESS              0
 
-typedef unsigned char  SdByte;
-typedef int            SdResult;
-typedef int            SdState;
-typedef int            SdHandle;
+typedef unsigned char   SdByte;
+typedef int             SdResult;
+typedef int             SdState;
+typedef int             SdHandle;
 
 class SerialDriver
 {
@@ -22,14 +22,14 @@ public:
     SerialDriver();
 
     ~SerialDriver();
-     
-    SdResult open_port( char *port_name );
-     
+
+    SdResult open_port(char *port_name);
+
     void close_port();
-    
-    SdResult write_to_port( SdByte *data, int length );
-    
-    SdResult read_from_port( SdByte *data, int max_length, SdState *state );
+
+    SdResult write_to_port(SdByte *data, int length);
+
+    SdResult read_from_port(SdByte *data, int max_length, SdState *state);
 };
 
 #endif // _SERIAL_DRIVER_H_
--- src/SerialDriver.cpp
+++ src/SerialDriver.cpp
@@ -20,14 +20,14 @@ SerialDriver::~SerialDriver()
 // reference URL: http://www.02.246.ne.jp/~torutk/cxx/serial/posix_serial.html,
 //                https://linuxjm.osdn.jp/html/LDP_man-pages/man3/termios.3.html
 
-SdResult SerialDriver::open_port( char *port_name )
+SdResult SerialDriver::open_port(char *port_name)
 {
     char path_to_port[SD_MAX_PORT_NAME_LEN + 6]; // +6 for "/dev/%s"
 
-    sprintf( path_to_port, "/dev/%s", port_name );
-    handle = open( path_to_port, O_RDWR | O_NOCTTY );
+    sprintf(path_to_port, "/dev/%s", port_name);
+    handle = open(path_to_port, O_RDWR | O_NOCTTY);
 
-    if ( handle >= 0 )
+    if (handle >= 0)
     {
         /*
         struct termios {
@@ -40,11 +40,11 @@ SdResult SerialDriver::open_port( char *port_name )
         */
         struct termios port_config;
 
-        if ( !tcgetattr( handle, &port_config ) )
+        if (!tcgetattr(handle, &port_config))
         {
             port_config.c_cflag = CS8 | CLOCAL | CREAD;
-            
-            switch( SD_SERIAL_SPEED )
+
+            switch (SD_SERIAL_SPEED)
             {
             case 9600:
                 port_config.c_cflag |= B9600;
@@ -61,109 +61,109 @@ SdResult SerialDriver::open_port( char *port_name )
             default:
                 port_config.c_cflag |= B115200;
                 break;
-			}
-			port_config.c_iflag = INPCK;
-			port_config.c_oflag = 0;
-			port_config.c_lflag = 0;
-			port_config.c_cc[VMIN] = 0;
-			port_config.c_cc[VTIME] = 0;
+            }
+            port_config.c_iflag = INPCK;
+            port_config.c_oflag = 0;
+            port_config.c_lflag = 0;
+            port_config.c_cc[VMIN] = 0;
+            port_config.c_cc[VTIME] = 0;
 
-			if ( !tcsetattr( handle, TCSANOW, &port_config ) )
-			{
-		    	return SD_SUCCESS;
-			}
+            if (!tcsetattr(handle, TCSANOW, &port_config))
+            {
+                return SD_SUCCESS;
+            }
         }
-    	close( handle );
+        close(handle);
     }
 
     return SD_ERROR;
-
 }
-     
+
 void SerialDriver::close_port()
 {
-	if ( handle != SD_INVALID_HANDLE )
+    if (handle != SD_INVALID_HANDLE)
     {
-        close( handle );
+        close(handle);
     }
     handle = SD_INVALID_HANDLE;
 }
 
-SdResult SerialDriver::write_to_port( SdByte *data, int length )
+SdResult SerialDriver::write_to_port(SdByte *data, int length)
 {
-	if ( tcflush( handle, TCIFLUSH ) )
+    if (tcflush(handle, TCIFLUSH))
     {
-    	return SD_ERROR;
+        return SD_ERROR;
     }
-    return write( handle, data, length );
+    return write(handle, data, length);
 }
 
 // reference URL: https://software.fujitsu.com/jp/manual/manualfiles/M050000/B1WN4911/01/idmgr05/idmgr378.htm,
 
-SdResult SerialDriver::read_from_port( SdByte *data, int max_length, SdState *state )
+SdResult SerialDriver::read_from_port(SdByte *data, int max_length, SdState *state)
 {
-	/*
+    /*
 	struct　timeval　{
 　　　　long　tv_sec;  //[s]
 　　　　long　tv_usec; //[s]
 　   };
 	*/
-	struct timeval timeout;
-	fd_set         fds;
-    SdResult       total_read_length = 0;
-    int            interval_timeout_ms = 20000000/SD_SERIAL_SPEED;
+    struct timeval timeout;
+    fd_set fds;
+    SdResult total_read_length = 0;
+    int interval_timeout_ms = 20000000 / SD_SERIAL_SPEED;
 
-	timeout.tv_sec  = 1;
+    timeout.tv_sec = 1;
     timeout.tv_usec = 0;
 
-	FD_ZERO( &fds );
-    FD_SET( handle, &fds );
+    FD_ZERO(&fds);
+    FD_SET(handle, &fds);
 
-	int select_result = select( handle + 1, &fds, NULL, NULL, &timeout );
-    if ( select_result <= 0 )
+    int select_result = select(handle + 1, &fds, NULL, NULL, &timeout);
+    if (select_result <= 0)
     {
         printf("\n SerialDriver.cpp select() has error %d \n", errno);
-    	return SD_ERROR;
+        return SD_ERROR;
     }
-    
-	//TODO: write a comment, why 2000? search interval time!
+
+    //TODO: write a comment, why 2000? search interval time!
     interval_timeout_ms = interval_timeout_ms < 2000 ? 2000 : interval_timeout_ms;
-    
-	int count = 0;
+
+    int count = 0;
     int maxCount = 100;
-    while( total_read_length < max_length )
+    while (total_read_length < max_length)
     {
-        if ( count > maxCount ) return SD_ERROR;
+        if (count > maxCount)
+            return SD_ERROR;
 
         timeout.tv_sec = 0;
         timeout.tv_usec = interval_timeout_ms;
 
-        FD_ZERO( &fds );
-        FD_SET( handle, &fds );
+        FD_ZERO(&fds);
+        FD_SET(handle, &fds);
 
-        *state = select( handle + 1, &fds, NULL, NULL, &timeout );
+        *state = select(handle + 1, &fds, NULL, NULL, &timeout);
 
-        if ( *state < 0 )
+        if (*state < 0)
         {
             printf("\n SerialDriver.cpp invalid state %d \n", *state);
-        	return SD_ERROR;
+            return SD_ERROR;
         }
-        else if ( *state == 0 )
+        else if (*state == 0)
         {
-        	return total_read_length;
+            return total_read_length;
         }
 
         int read_length;
 
-    	read_length = read( handle, data + total_read_length, max_length-total_read_length );
+        read_length = read(handle, data + total_read_length, max_length - total_read_length);
 
-    	if ( read_length < 0 )
-    	{
+        if (read_length < 0)
+        {
             printf("\n SerialDriver.cpp read faled total_read_length: %d \n", total_read_length);
-    		return SD_ERROR;
-    	}
+            return SD_ERROR;
+        }
 
-    	total_read_length += read_length;
+        total_read_length += read_length;
         ++count;
     }
 
